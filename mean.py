83b628884da1091c9a061ee9f8427609c9f5a977
SY348 <105688871+SY348@users.noreply.github.com>
2024-05-30 16:53:37 +0800

    mean.py

--- mean.py
+++ mean.py
@@ -5,27 +5,14 @@ import sys
 import pandas as pd
 import scipy.stats as stats
 
-if len(sys.argv) != 3:
-	print("Usage: python mean.py <sorted.pA> <sorted.pG>")
-	sys.exit(1)
-
 pd.set_option('display.max_rows',None)
 
-inputfile_A = sys.argv[1]
-inputfile_A = pd.read_csv(inputfile_A, sep='\t', header=None)
-inputfile_A.columns = ['gene_name', 'pA_number']
+inputfile = sys.argv[1]
+inputfile = pd.read_csv(inputfile, sep='\t', header=None)
+inputfile.columns = ['gene_name', 'pA_length']
 
-inputfile_G = sys.argv[2]
-inputfile_G = pd.read_csv(inputfile_G, sep='\t', header=None)
-inputfile_G.columns = ['gene_name', 'pG_number']
 
-for line in inputfile_A:
-	A_mean = inputfile_A.groupby('gene_name')['pA_number'].mean().reset_index()
-
-for line in inputfile_G:
-	G_mean = inputfile_G.groupby('gene_name')['pG_number'].mean().reset_index()
-
-merge = pd.merge(A_mean, G_mean, on='gene_name')
-merge['G%']=merge['pG_number']/(merge['pA_number']+merge['pG_number']+1)*100
-
-print (merge)
+for line in inputfile:
+	input_mean = inputfile.groupby('gene_name')['pA_length'].mean().reset_index()
+	input_mean['pA_length'] = input_mean['pA_length'].round()
+	print(input_mean)

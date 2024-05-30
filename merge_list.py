f71cb6f102b339f31b580e0c99fe3b91dd609fed
SY348 <105688871+SY348@users.noreply.github.com>
2024-05-30 16:07:56 +0800

    merge_list.py

--- merge_list.py
+++ merge_list.py
@@ -8,15 +8,16 @@ if len(sys.argv) != 3:
 	sys.exit(1)
 
 whole_list = sys.argv[1]
-whole_list = pd.read_csv(whole_list, sep='\t')
+whole_list = pd.read_csv(whole_list)
 
 filenames = sys.argv[2]
 filenames = pd.read_csv(filenames, header=None)[0].tolist()
 
+
 for i in filenames:
 	temp = pd.read_csv(i, sep='\t')
-	whole_list= pd.merge(whole_list, temp,on='nam', how='outer')
+	whole_list= pd.merge(whole_list, temp, on='gene_name', how='outer')
 
 whole_list.fillna(0,inplace=True)
 
-print(whole_list£©
+print(whole_list)

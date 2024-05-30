426829b1b0731b6bad7981678584918655e6e113
SY348 <105688871+SY348@users.noreply.github.com>
2024-05-30 20:40:50 +0800

    merge_list.py

--- merge_list.py
+++ merge_list.py
@@ -1,25 +1,31 @@
+#!/usr/bin/env python
+
 import sys
 import os
+import openpyxl
 import pandas as pd
 import numpy as np
 
+sys.setrecursionlimit(10000)
 pd.set_option('display.max_rows',None)
+pd.set_option('display.max_columns',None)
 
 if len(sys.argv) != 3:
 	print("Usage: python merge_list.py <total_list> <file_name_list>")
 	sys.exit(1)
 
-whole_list = sys.argv[1]
-whole_list = pd.read_csv(whole_list)
+whole_list_file = sys.argv[1]
+whole_list = pd.read_csv(whole_list_file)
 
-filenames = sys.argv[2]
-filenames = pd.read_csv(filenames, header=None)[0].tolist()
+filenames_file = sys.argv[2]
+filenames = pd.read_csv(filenames_file, header=None)[0].tolist()
 
+merge_df = whole_list.copy()
 
-for i in filenames:
-	temp = pd.read_csv(i, sep='\t')
-	whole_list= pd.merge(whole_list, temp, on='gene_name', how='outer')
+for filename in filenames:
+	temp_df = pd.read_csv(filename, sep='\t')
+	merge_df= pd.merge(merge_df, temp_df, on='gene_name', how='outer')
 
-whole_list.fillna(0,inplace=True)
+merge_df.fillna(0,inplace=True)
 
-print(whole_list)
+merge_df.to_csv('_merge.pA.txt', sep='\t',index= False)

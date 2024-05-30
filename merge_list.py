0cc8b528353e11deb16e494fcc44aa01294968c5
SY348 <105688871+SY348@users.noreply.github.com>
2024-05-30 16:10:15 +0800

    merge_list.py

--- merge_list.py
+++ merge_list.py
@@ -3,6 +3,8 @@ import os
 import pandas as pd
 import numpy as np
 
+pd.set_option('display.max_rows',None)
+
 if len(sys.argv) != 3:
 	print("Usage: python merge_list.py <total_list> <file_name_list>")
 	sys.exit(1)
